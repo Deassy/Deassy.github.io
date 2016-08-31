7aad92e63dfd2d8881106dc8ac0261c0ee3fb16c
Deassy <deassy999@gmail.com>
2016-08-31 20:00:48 +0700

    revise responsee.js

--- js/responsee.js
+++ js/responsee.js
@@ -1,9 +1,4 @@
-/*
- * Responsee JS - v4 - 2016-06-30
- * https://www.myresponsee.com
- * Copyright 2016, Vision Design - graphic zoo
- * Free to use under the MIT license.
-*/
+
 jQuery(document).ready(function($) {
   //Responsee tabs
   $('.tabs').each(function(intex, element) {
